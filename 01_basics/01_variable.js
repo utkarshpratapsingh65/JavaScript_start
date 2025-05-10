91b1e69a9cf9f81897acc3a85a48260bf60e6dc3
Utkarsh Pratap Singh <128781572+utkarshpratapsingh65@users.noreply.github.com>
2025-05-10 14:36:41 +0000

    contain config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,5 @@
+const accountID = 123456
+let acc_Email = "using.singh@gmail.com"
+var acc_Password = "123456"
+console.log(accountID);
+console.table([acc_Email, acc_Password, accountID]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,3 @@
+"use strick";//treat all js as newer version
+// alert(3+3) // we are using nodejs, not browser
+ 
